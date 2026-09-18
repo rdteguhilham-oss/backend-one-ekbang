08f2ba6317ce640ede46ff1a8e97759ddc255f70
Teguh Ilham <rd.teguhilham@gmail.com>
2026-09-18 22:39:22 +0700

    Menyesuaikan port untuk Railway

--- server.js
+++ server.js
@@ -378,7 +378,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ status: 'error', pesan: 'Kesalahan sistem saat memproses.' });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server digembok dan berjalan aman di http://localhost:${PORT}`);
+    console.log(`Server digembok dan berjalan aman di port ${PORT}`);
 });
